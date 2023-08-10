c6263d3438fd0d150528f6922c01f62b31d4b705
Emerson <emersonlucenaoficial@gmail.com>
2023-08-10 14:35:29 -0300

    feat: add functionality including the frontend part

---
+++ index.mjs
@@ -0,0 +1,61 @@
+import OBSWebSocket from "obs-websocket-js";
+const obs = new OBSWebSocket();
+import express from "express";
+import { engine } from "express-handlebars";
+import WebSocket from "ws";
+import dotenv from "dotenv";
+
+//made by @erms1337
+
+try {
+  dotenv.config();
+  const app = express();
+  const wss = new WebSocket.Server({
+    port: 8080,
+  });
+
+  app.engine("handlebars", engine());
+  app.set("view engine", "handlebars");
+  app.set("views", "./views");
+
+  app.get("/", async (req, res) => {
+    const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(
+      `ws://127.0.0.1:${process.env.OBS_PORT}`,
+      process.env.OBS_PASSWORD,
+      { protocols: "obs-websocket" }
+    );
+
+    console.log(
+      `Connected to server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`
+    );
+
+    const firstScreenshot = await obs.call("GetSourceScreenshot", {
+      sourceName: "scene",
+      imageFormat: "jpeg",
+    });
+
+    wss.on("connection", function connection(ws) {
+      ws.on("message", async function incoming(d) {
+        const { imageData } = await obs.call("GetSourceScreenshot", {
+          sourceName: "scene",
+          imageFormat: "jpeg",
+        });
+        ws.send(JSON.stringify({ imageEncoded: imageData }));
+      });
+    });
+
+    res.render("home", {
+      title: "Handlebars Express Demo",
+      imageEncoded: firstScreenshot.imageData,
+    });
+  });
+
+  const PORT = process.env.PORT || 8989;
+  app.listen(PORT, () => {
+    console.log(
+      `Server is listening on port ${PORT}: http://localhost:${PORT}`
+    );
+  });
+} catch (error) {
+  console.error("Failed to connect", error.code, error.message);
+}
